7c3419d5041e7dc680007aded0dcbf0ab63e358a
schellse <semi.schell@t-online.de>
2022-08-17 18:13:15 +0200

    main.py: Include test_regression_model in _steps basic_cleaning/run.py: Exclude outliers for Longitude & Latitude

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -38,6 +38,10 @@ def go(args):
     idx = df['price'].between(min_price, max_price)
     df = df[idx].copy()
     
+    # Drop outliers (new in Release 1.0.1)
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Convert last_review to datetime
     logger.info("Convert last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
